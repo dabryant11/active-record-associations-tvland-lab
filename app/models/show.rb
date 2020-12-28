d6b8686ab6c0faae034fb5dd796971818164dcbf
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2020-12-28 11:55:54 -0500

    1155

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
   
+    has_many :characters 
+    has_many :actors, through: :characters 
+
+    def actors_list 
+        # self.characters.actors.select {|actors| self.characters.actors.full_name}
+
+        self.actors.map{|actor|actor.full_name}
+    end 
 end
